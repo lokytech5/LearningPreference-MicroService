92aef8ab36538ea7fcec88a8e6403198ff7f3a71
lokytech5 <lokytech5@gmail.com>
2023-10-10 18:48:41 +0100

    Initial Commit

---
+++ src/main/java/com/lokytech/learningPreferenceservice/LearningPreferenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lokytech.learningPreferenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPreferenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPreferenceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokytech/learningPreferenceservice/LearningPreferenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokytech.learningPreferenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPreferenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
