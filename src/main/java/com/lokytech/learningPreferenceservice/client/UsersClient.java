7a4700b454f30f2a3ba6e407086230137ba43c0b
lokytech5 <lokytech5@gmail.com>
2023-10-10 22:10:46 +0100

    Added feign Client configuration to communicate with user-service

--- src/main/java/com/lokytech/learningPreferenceservice/client/UsersClient.java
+++ src/main/java/com/lokytech/learningPreferenceservice/client/UsersClient.java
@@ -1,8 +1,13 @@
 package com.lokytech.learningPreferenceservice.client;
 
+import com.lokytech.learningPreferenceservice.dto.UsersDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "users-service")
 public interface UsersClient {
-    
+
+    @GetMapping("/api/users/{id}")
+     UsersDTO getUserId(@PathVariable("id") Long id);
 }
