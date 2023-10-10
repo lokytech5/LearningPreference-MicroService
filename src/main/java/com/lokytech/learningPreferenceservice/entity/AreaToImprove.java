4d255bb98b008e0df1c92b33f586023add727ddc
lokytech5 <lokytech5@gmail.com>
2023-10-10 22:21:27 +0100

    Added setters, getters and constructor to AreaToImprove class

--- src/main/java/com/lokytech/learningPreferenceservice/entity/AreaToImprove.java
+++ src/main/java/com/lokytech/learningPreferenceservice/entity/AreaToImprove.java
@@ -15,4 +15,37 @@ public class AreaToImprove {
     private UsersDTO usersDTO;
 
     private String areaType;
+
+    public AreaToImprove() {
+    }
+
+    public AreaToImprove(Long areaId, UsersDTO usersDTO, String areaType) {
+        this.areaId = areaId;
+        this.usersDTO = usersDTO;
+        this.areaType = areaType;
+    }
+
+    public Long getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Long areaId) {
+        this.areaId = areaId;
+    }
+
+    public UsersDTO getUsersDTO() {
+        return usersDTO;
+    }
+
+    public void setUsersDTO(UsersDTO usersDTO) {
+        this.usersDTO = usersDTO;
+    }
+
+    public String getAreaType() {
+        return areaType;
+    }
+
+    public void setAreaType(String areaType) {
+        this.areaType = areaType;
+    }
 }
