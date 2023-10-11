7d38ed6c8f118f5c93059b25613e101920c08bf2
lokytech5 <lokytech5@gmail.com>
2023-10-11 13:32:24 +0100

    Added save learningPreference method to service and createdPreferences endpoint to controller

--- src/main/java/com/lokytech/learningPreferenceservice/controller/LearningPreferenceController.java
+++ src/main/java/com/lokytech/learningPreferenceservice/controller/LearningPreferenceController.java
@@ -1,10 +1,25 @@
 package com.lokytech.learningPreferenceservice.controller;
 
 
+import com.lokytech.learningPreferenceservice.entity.LearningPreference;
+import com.lokytech.learningPreferenceservice.service.LearningPreferenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
 public class LearningPreferenceController {
 
+    @Autowired
+    LearningPreferenceService preferenceService;
+
+    @PostMapping("/learningPreference")
+    public ResponseEntity<LearningPreference> createLearningPreference(@RequestBody LearningPreference learningPreference){
+        LearningPreference preference = preferenceService.savePreference(learningPreference);
+        return new ResponseEntity<>(preference, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/lokytech/learningPreferenceservice/service/LearningPreferenceService.java
+++ src/main/java/com/lokytech/learningPreferenceservice/service/LearningPreferenceService.java
@@ -1,6 +1,16 @@
 package com.lokytech.learningPreferenceservice.service;
+import com.lokytech.learningPreferenceservice.entity.LearningPreference;
+import com.lokytech.learningPreferenceservice.repository.LearningPreferenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LearningPreferenceService {
+
+    @Autowired
+    LearningPreferenceRepository preferenceRepository;
+
+    public LearningPreference savePreference(LearningPreference learningPreference){
+        return preferenceRepository.save(learningPreference);
+    }
 }
